c4c830dd9892a968093459d6daf9dec30a96914e
Philip Howley <howley.phil@gmail.com>
2024-04-27 13:36:06 +0100

    add parcel

---
+++ week-02/01-movie-review/assets/js/script.js
@@ -0,0 +1,9 @@
+import Swal from 'sweetalert2'
+
+document.getElementById("showAlert").addEventListener("click", function () {
+  Swal.fire("Hello from SweetAlert2!");
+});
+
+document.getElementById("showAlert").addEventListener("click", function () {
+  Swal.fire("Hello from SweetAlert2!");
+});
